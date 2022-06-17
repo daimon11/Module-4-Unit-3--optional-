c435b4db44acf79f0296695046709ca64f4e9027
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-06-17 22:35:45 +0300

    17-06-22(22-35)

--- 1/tasksHard_1.js
+++ 1/tasksHard_1.js
@@ -2,10 +2,8 @@ const income = +prompt('Ваш доход в рублях', '');
 
 if (income <= 15000) {
   alert('Налог = ' + income * 0.12);
-}
-if (income > 15000 && income < 50000) {
+} else if (income > 15000 && income < 50000) {
   alert('Налог = ' + income * 0.2);
-}
-if (income >= 50000) {
+} else if (income >= 50000) {
   alert('Налог = ' + income * 0.3);
 }
